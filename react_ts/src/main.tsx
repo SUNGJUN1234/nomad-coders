3faac7706216fb99aa74ec955d946498a821d14d
SUNGJUN1234 <chotjdwns654@gmail.com>
2024-07-17 16:29:26 +0900

    pnpm, recoil 설치

--- react_ts/src/main.tsx
+++ react_ts/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import {RecoilEnv, RecoilRoot} from "recoil";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
+ReactDOM.createRoot(document.getElementById("container") as HTMLElement).render(
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
-)
+  </RecoilRoot>
+);
